dbe87189b9c6d4fa61a226ab8df857e3f36d5c4b
sudeciiftci <sudeciiftci@gmail.com>
2026-04-24 13:10:56 +0300

    first commit

---
+++ main/packages2.java
@@ -0,0 +1,10 @@
+package main;
+
+import school.packages;
+
+public class packages2 {
+    public static void main(String[] args) {
+        packages p = new packages();
+        p.show();
+    }  
+}
---
+++ school/packages.java
@@ -0,0 +1,11 @@
+package school;
+
+
+public class packages{
+
+    public void show(){
+        System.out.println("Hello");
+    } 
+}
+
+
